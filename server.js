a5ccf47fe7e3f408430fb1a3abb99dd749a1925b
githubrgb <36651964+githubrgb@users.noreply.github.com>
2018-04-03 19:01:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article-one', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function(req,res){
-    res.send('article two requested and will be served here')
+    res.send('article two requested and will be served here');
 });
 app.get('/article-three', function(req,res){
-    res.send('article three requested and will be served here')
+    res.send('article three requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
