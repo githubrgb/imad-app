e52b8430b555476f6fff7d0787911fda7717aa8c
githubrgb <36651964+githubrgb@users.noreply.github.com>
2018-04-03 18:10:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one', function(req,res){
+    res.send('article one requested and will be served here')
+});
+app.get('/article-two', function(req,res){
+    res.send('article two requested and will be served here')
+});
+app.get('/article-three', function(req,res){
+    res.send('article three requested and will be served here')
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
